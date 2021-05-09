97023cae125b3ad776a0fd551ac31e661bf21bd7
Tiffany Cole <Drakenshiinx@hotmail.com>
2021-05-09 12:38:32 -0700

    added action to WebApiConfig file

--- TiffanyValentinaHW4-ef-sol/TiffanyValentinaHW4-ef/App_Start/WebApiConfig.cs
+++ TiffanyValentinaHW4-ef-sol/TiffanyValentinaHW4-ef/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace TiffanyValentinaHW4_ef
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
